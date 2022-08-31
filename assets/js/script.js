035f62d3fa50862c7519b76aaffed0ba9593e213
GH0ST <adamjamessherrill@gmail.com>
2022-08-30 19:18:46 -0500

    added colorBlock function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,27 @@
-// Adds live time to schedule
-var currentDay = moment().format('MMMM Do YYYY');
-$('#currentDay').text(currentDay);
+// Displays current date and time in header
+var currentDate = moment().format("MMMM Do YYYY hA");
+$("#currentDay").text(currentDate);
 
-// not working
-var allBlocks = {
-    block1: $('#9-am'),
-    block2: $('#10-am'),
-    block3: $('#11-am'),
-    block4: $('#12-pm'),
-    block5: $('#1-pm'),
-    block6: $('#2-pm'),
-    block7: $('#3-pm'),
-    block8: $('#4-pm'),
-    block9: $('#5-pm'),
-}
+function colorBlock() {
+  var currentTime = moment().hours(); // Military time
 
-console.log(allBLocks);
+  let timeBlocks = $(".time-block");
 
-allBLocks.block1.innerHTML = moment('9AM', 'HA');
-allBlocks.block2.innerHTML = moment('10AM', 'HA');
-allBLocks.block3.innerHTML = moment('11AM', 'HA');
-allBLocks.block4.innerHTML = moment('12PM', 'HA');
-allBLocks.block5.innerHTML = moment('13PM', 'HA');
-allBLocks.block6.innerHTML = moment('14PM', 'HA');
-allBLocks.block7.innerHTML = moment('15PM', 'HA');
-allBLocks.block8.innerHTML = moment('16PM', 'HA');
-allBLocks.block9.innerHTML = moment('17PM', 'HA');
+  for (let i = 0; i < timeBlocks.length; i++) {
+    let timeBlockId = timeBlocks[i].getAttribute("id");
+    let militaryTime = parseInt(timeBlockId.replace("hour", "")) + 8;
+    let textareaEl = timeBlocks[i].children[1];
 
-for (var i = 0; i < allBlocks.length; i++) {
-    if (moment().isSame(allBlocks.innerHTML)) {
-        $(textarea[i]).addClass('present');
-    } else if (moment().isBefore(allBlocks.innerHTML)) {
-        $(textarea[i]).addClass('future');
-    } else if (moment().isAfter(allBlocks.innerHTML)) {
-        $(textarea[i]).addClass('past');
+    if (militaryTime < currentTime) {
+      textareaEl.classList.add("past");
     }
+    if (militaryTime === currentTime) {
+      textareaEl.classList.add("present");
+    }
+    if (militaryTime > currentTime) {
+      textareaEl.classList.add("future");
+    }
+  }
 }
 
-
-
+colorBlock();
